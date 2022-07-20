7f4ce58b36e412d22e0634da0826b10e37fdee60
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-07-20 16:15:00 -0400

    fixed port for heroku, added listen function

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 import express from "express";
 
 const app = express();
-const port = 3003;
+const port = process.env.port || 3003
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
+
+app.listen(port, () =>
+    console.log(`App listening at http://localhost:${port}`)
+);
