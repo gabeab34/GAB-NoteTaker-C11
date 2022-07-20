46fadfe6fd1cafc90711873957fc09c569b4ba6e
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-07-20 18:17:56 -0400

    get started button path works

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const port = process.env.port || 3003
+const __dirname = path.resolve()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+    res.sendFile((__dirname + '/public/index.html'))
+);
 
 app.listen(port, () =>
     console.log(`App listening at http://localhost:${port}`)
